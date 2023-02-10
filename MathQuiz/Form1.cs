9b1d48515cdbf9ade87ad6c178c3820f423fe3db
USER <USER@DESKTOP-TRQG01D>
2023-02-10 13:08:20 +0300

    Добавьте файлы проекта.

---
+++ MathQuiz/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+namespace MathQuiz
+{
+    public partial class Form1 : Form
+
+    {
+        Random randomizer = new Random();
+        int addend1;
+        int addend2;
+        /// <summary>
+        /// Start the quiz by filling in all of the problems
+        /// and starting the timer.
+        /// </summary>
+        // These integer variables store the numbers 
+        // for the subtraction problem. 
+        int minuend;
+        int subtrahend;
+        // These integer variables store the numbers 
+        // for the multiplication problem. 
+        int multiplicand;
+        int multiplier;
+        // These integer variables store the numbers 
+        // for the division problem. 
+        int dividend;
+        int divisor;
+        // remaining time.
+        int timeLeft;
+        public void StartTheQuiz()
+        {
+            // Fill in the addition problem.
+            // Generate two random numbers to add.
+            // Store the values in the variables 'addend1' and 'addend2'.
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            // Convert the two randomly generated numbers
+            // into strings so that they can be displayed
+            // in the label controls.
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            // 'sum' is the name of the NumericUpDown control.
+            // This step makes sure its value is zero before
+            // adding any values to it.
+            sum.Value = 0;
+            // Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+
+        }
+        /// <summary>
+        /// Check the answers to see if the user got everything right.
+        /// </summary>
+        /// <returns>True if the answer's correct, false otherwise.</returns>
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+
+        private void CheckTheAnswerAndDoVoice(int type = 0)
+        {
+            Boolean isCorrect = false;
+            switch (type)
+            {
+                case 0:
+                    isCorrect = addend1 + addend2 == sum.Value; break;
+                case 1: isCorrect = minuend - subtrahend == difference.Value; break;
+                case 2: isCorrect = (multiplicand * multiplier == product.Value); break;
+                case 3: isCorrect = (dividend / divisor == quotient.Value); break;
+
+            }
+            if (!isCorrect)
+            {
+                System.Media.SoundPlayer player =
+                    new System.Media.SoundPlayer(@"d:\sounds1\error.wav");
+                player.Play();
+            }
+            else
+            {
+                System.Media.SoundPlayer player =
+                    new System.Media.SoundPlayer(@"d:\sounds1\success.wav");
+                player.Play();
+
+            }
+        }
+        public Form1()
+        {
+            // Create a Random object called randomizer 
+            // to generate random numbers.
+            InitializeComponent();
+        }
+        public void StartTheQuizi()
+        {
+            // Fill in the addition problem.
+            // Generate two random numbers to add.
+            // Store the values in the variables 'addend1' and 'addend2'.
+            
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            // Convert the two randomly generated numbers
+            // into strings so that they can be displayed
+            // in the label controls.
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            // 'sum' is the name of the NumericUpDown control.
+            // This step makes sure its value is zero before
+            // adding any values to it.
+            sum.Value = 0;
+            // Fill in the subtraction problem.
+            int minuend;
+            int subtrahend;
+            int multiplicand;
+            int multiplier;
+            int dividend;
+            int divisor;
+            int timeLeft;
+            // Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+            bool CheckTheAnswer()
+            {
+                if ((addend1 + addend2 == sum.Value)
+                    && (minuend - subtrahend == difference.Value)
+                    && (multiplicand * multiplier == product.Value)
+                    && (dividend / divisor == quotient.Value))
+                    return true;
+                else
+                    return false;
+            }
+
+            
+            void timer1_Tick(object sender, EventArgs e)
+            {
+                        
+                
+                if (CheckTheAnswer())
+                {
+                    // If CheckTheAnswer() returns true, then the user 
+                    // got the answer right. Stop the timer  
+                    // and show a MessageBox.
+                    timer1.Stop();
+                    MessageBox.Show("You got all the answers right!",
+                                    "Congratulations!");
+                    startButton.Enabled = true;
+                }
+                else if (timeLeft > 0)
+                {
+                    // If CheckTheAnswer() returns false, keep counting
+                    // down. Decrease the time left by one second and 
+                    // display the new time left by updating the 
+                    // Time Left label.
+                    
+                    timeLeft = timeLeft - 1;
+                    timeLabel.Text = timeLeft + " seconds";
+                }
+                else
+                {
+                    // If the user ran out of time, stop the timer, show
+                    // a MessageBox, and fill in the answers.
+                    timer1.Stop();
+                    timeLabel.Text = "Time's up!";
+                    MessageBox.Show("You didn't finish in time.", "Sorry!");
+                    sum.Value = addend1 + addend2;
+                    difference.Value = minuend - subtrahend;
+                    product.Value = multiplicand * multiplier;
+                    quotient.Value = dividend / divisor;
+                    startButton.Enabled = true;
+                }
+                
+
+
+            }
+        }
+        private void plusLeftLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                // If CheckTheAnswer() returns false, keep counting
+                // down. Decrease the time left by one second and 
+                // display the new time left by updating the 
+                // Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show
+                // a MessageBox, and fill in the answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
+        }
+
+        private void sum_ValueChanged(object sender, EventArgs e)
+        {
+            CheckTheAnswerAndDoVoice(0);
+        }
+
+        private void difference_ValueChanged(object sender, EventArgs e)
+        {
+            CheckTheAnswerAndDoVoice(1);
+        }
+
+        private void difference_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+
+        }
+
+        private void product_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+
+        }
+
+        private void quotient_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+
+        }
+
+        private void product_ValueChanged(object sender, EventArgs e)
+        {
+            CheckTheAnswerAndDoVoice(2);
+        }
+
+        private void quotient_ValueChanged(object sender, EventArgs e)
+        {
+            CheckTheAnswerAndDoVoice(3);
+        }
+    }
+}
+  
+
+
